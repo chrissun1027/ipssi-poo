1b8015f8ffde7d0becfcc86675dc48e0d753413a
chrissun1027 <56598797+chrissun1027@users.noreply.github.com>
2019-11-15 16:33:34 +0100

    sun jiamin

--- exercice1.php
+++ exercice1.php
@@ -8,7 +8,6 @@ class Diviseur {
     public function division(int $index, int $diviseur)
     {
         $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
-
         return $valeurs[$index] / $diviseur;
     }
 }
---
+++ src/Couleur.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Ipssi\Evaluation;
+
+
+class Couleur
+{
+
+}
+
+
+
+git config --global user.email "chrisun2710@gmail.com"
+  git config --global user.name "chrissun2710"
